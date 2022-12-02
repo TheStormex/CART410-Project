e1b6e5c33dc727d1172d3fe19bd972739e717d1e
TheStormex <55155047+TheStormex@users.noreply.github.com>
2022-12-01 22:36:11 -0500

    added the objects in gamemanager

---
+++ CART410-Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // UI elements
+    public Canvas textBoxCanvas;
+    public Text textBoxDialogue;
+    public Text textBoxName;
+    public Button textBoxButton1;
+    public Button textBoxButton2;
+
+    // music and sounds
+     public AudioSource soundWalk;
+     public AudioSource soundSong;
+     public AudioSource soundIntObject;
+     public AudioSource soundIntPerson;
+     public AudioSource soundHover;
+     public AudioSource soundClick;
+     public AudioSource soundEnd;
+
+    // Interactables
+
+    // objects
+
+
+    // characters
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // play the sounds
+     public void playSoundWalk()
+    {
+        soundWalk.Play();
+    }
+     public void playSoundIntObject()
+    {
+        soundIntObject.Play();
+    }
+     public void playSoundIntPerson()
+    {
+        soundIntPerson.Play();
+    }
+     public void playSoundHover()
+    {
+        soundHover.Play();
+    }
+     public void playsoundClick()
+    {
+        soundClick.Play();
+    }
+     public void playSoundEnd()
+    {
+        soundEnd.Play();
+    }
+}
--- CART410-Project/Assets/Scripts/Interactable.cs
+++ CART410-Project/Assets/Scripts/Interactable.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Renderer))]
+public abstract class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
